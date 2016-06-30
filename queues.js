d8381dcdf7c1d9cf1b0372517302ec8a84fd460f
mrezo <balarezo.m@gmail.com>
2016-06-30 09:02:29 -0500

    Completes queue constructor

--- queues.js
+++ queues.js
@@ -6,5 +6,27 @@
 var Queue = function () {
   this.newSize = 0;
   this.oldSize = 0;
-  
+  this.storage = {};
 }
+
+Queue.prototype.size = function() {
+  return Object.keys(this.storage).length;
+}
+
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.newSize] = value;
+  this.newSize++
+}
+
+Queue.prototype.dequeue = function() {
+  if (this.size() > 0) {
+    var removed = this.storage[this.oldSize];
+    delete this.storage[this.oldSize];
+    this.oldSize++
+    return removed;
+  } else {
+    return 'Cannot dequeue from an empty queue'
+  }
+};
+module.exports = Queue
+

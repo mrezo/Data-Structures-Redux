6d78d259dcc6209a123d12cdae3a8a4e79f4152a
mrezo <balarezo.m@gmail.com>
2016-06-29 21:57:34 -0500

    Begins queue constructor function

---
+++ queues.js
@@ -0,0 +1,10 @@
+// Queues are used for organizing data in a way that 
+// allows for the first item in the queue to be the first
+// item out of the queue. Think of it as just like a line
+// at the movie theater.
+
+var Queue = function () {
+  this.newSize = 0;
+  this.oldSize = 0;
+  
+}
